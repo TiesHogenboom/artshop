9f20a432428c877113a180b223e8f80695db9e19
29147 <29147@ma-web.nl>
2021-12-01 12:31:00 +0100

    Nieuwe Bestanden toegevoegd *VIDEO 1 t/m 7*

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
-if ( have_posts() ) {
+    get_header();
+?>
+<?php
 
-	// Load posts loop.
-	while ( have_posts() ) {
-		the_post();
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post(); ?>
 
-		the_content();
-    }
+    <h2><?php the_title() ?></h2>
+    <?php the_content() ?>
+    <a href="<?php the_permalink()?>">Lees Meer</a>
+    <hr>
 
+    <?php endwhile;
 
+else :
+    echo '<p> there are no posts!</p>';
 
-} ?>
-</body>
-</html>
+endif;
+
+?>
+
+<?php
+    get_header();
+?>
